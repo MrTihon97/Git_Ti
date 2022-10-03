1cecbb606d078bb80ee9e544b199ac9d8989c69c
MrTihon <tihonsomov@mail.ru>
2022-10-03 11:47:08 +0300

    add task_002

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Это число больше => {number1}");
+}
+else Console.WriteLine($"Это число больше => {number2}");
